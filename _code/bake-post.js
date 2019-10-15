54d2291d4fd6279a9cd9f00dcd2d99b483ef9990
Jayesh Salvi <jayeshsalvi@gmail.com>
2019-10-14 21:53:48 -0400

    post baking code working

--- _code/bake-post.js
+++ _code/bake-post.js
@@ -17,53 +17,87 @@ if(!fs.existsSync(filepath)) {
   process.exit(0);
 }
 
-// Open template files
-let postHeadTemplate = fs.readFileSync("post-head-template.html").toString();
-let postBodyTemplate = fs.readFileSync("post-body-template.html").toString();
-
-let mdInput = fs.readFileSync(filepath).toString();
-
-// Parse post filepath and deduce it's date
-let lastSlashIndex = filepath.lastIndexOf("/");
-let filename = filepath.substring(lastSlashIndex+1);
-let regex = /^(\d+)-(\d+)-(\d+)-([a-zA-Z0-9\-]+)/;
-
-if(!regex.test(filename)) {
-  console.error("Filename has wrong format");
-  process.exit(0);
+if(fs.lstatSync(filepath).isDirectory()) {
+  let filepaths = fs.readdirSync(filepath);
+  for(let fname of filepaths) {
+    bakePost(filepath+path.sep+fname);
+  }
+} else {
+  bakePost(filepath);
 }
 
-let match = regex.exec(filename);
-let yearStr = match[1];
-let monthStr = match[2];
-let month = parseInt(monthStr);
-let dayStr = match[3];
-let dashedPostName = match[4];
+function bakePost(mdFilepath) {
 
-let dateString = months[month-1]+" "+parseInt(dayStr)+", "+yearStr;
 
-// Generate html from markdown
-let converter = new showdown.Converter();
-let htmlOutput = converter.makeHtml(mdInput);
+  // Open template files
+  let postHeadTemplate = fs.readFileSync("post-head-template.html").toString();
+  let postBodyTemplate = fs.readFileSync("post-body-template.html").toString();
 
-let postHead = postHeadTemplate.replace('$TITLE$',dashedPostName);
+  let mdInput = fs.readFileSync(mdFilepath).toString();
 
-let postBody = postBodyTemplate.replace('$ARTICLE$',htmlOutput);
-postBody = postBody.replace('$POSTDATE$', dateString);
+  // Parse post mdFilepath and deduce it's date
+  let lastSlashIndex = mdFilepath.lastIndexOf("/");
+  let filename = mdFilepath.substring(lastSlashIndex+1);
+  let regex = /^(\d+)-(\d+)-(\d+)-([a-zA-Z0-9\-]+)/;
 
-// Create directory structure, if necessary
-let outpath = ""+yearStr;
-if(!fs.existsSync(outpath)) {
-  fs.mkdirSync(outpath);
+  if(!regex.test(filename)) {
+    console.error("Filename has wrong format");
+    process.exit(0);
+  }
+
+  let match = regex.exec(filename);
+  let yearStr = match[1];
+  let monthStr = match[2];
+  let month = parseInt(monthStr);
+  let dayStr = match[3];
+  let dashedPostName = match[4];
+
+  let dateString = months[month-1]+" "+parseInt(dayStr)+", "+yearStr;
+
+  // Extract title of the post
+  let postTitle = null;
+  let mdBody = "";
+  let bodyStart = false;
+  for(let line of mdInput.split(/\r?\n/)) {
+    if(bodyStart) {
+      mdBody += line+"\n";
+    } else {
+      if(line.trim().length > 0 && postTitle === null) {
+        postTitle = line;
+      }
+      if(line.trim() == "===") {
+        bodyStart = true;
+      }
+    }
+  }
+
+  // Generate html from markdown
+  let converter = new showdown.Converter();
+
+  let postHead = postHeadTemplate.replace(/\$TITLE\$/g,postTitle);
+  let htmlOutput = converter.makeHtml(mdBody);
+
+  let postBody = postBodyTemplate.replace('$ARTICLE$',htmlOutput);
+  postBody = postBody.replace('$POSTDATE$', dateString);
+  postBody = postBody.replace(/\$TITLE\$/g,postTitle);
+
+  // Create directory structure, if necessary
+  let outpath = ""+yearStr;
+  if(!fs.existsSync(outpath)) {
+    fs.mkdirSync(outpath);
+  }
+  outpath += path.sep+monthStr;
+  if(!fs.existsSync(outpath)) {
+    fs.mkdirSync(outpath);
+  }
+  outpath += path.sep+dayStr;
+  if(!fs.existsSync(outpath)) {
+    fs.mkdirSync(outpath);
+  }
+  outpath += path.sep+dashedPostName+".html";
+
+  fs.writeFileSync(outpath, postHead+"\n"+postBody);
+
+  console.log("Output html: "+outpath);
 }
-outpath += path.sep+monthStr;
-if(!fs.existsSync(outpath)) {
-  fs.mkdirSync(outpath);
-}
-outpath += path.sep+dayStr;
-if(!fs.existsSync(outpath)) {
-  fs.mkdirSync(outpath);
-}
-outpath += path.sep+dashedPostName+".html";
 
-fs.writeFileSync(outpath, postHead+"\n"+postBody);
