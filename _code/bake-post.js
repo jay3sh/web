4d031e33df31a2e30697aaa70c25c10d277c9a62
Jayesh Salvi <jay3sh@users.noreply.github.com>
2020-04-11 21:24:02 -0400

    new post

--- _code/bake-post.js
+++ _code/bake-post.js
@@ -3,32 +3,32 @@ const showdown = require("showdown");
 const path = require("path");
 
 const months = [
-  'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
 ];
 
 
-if(process.argv.length < 3) {
+if (process.argv.length < 3) {
   console.error("Missing input filepath");
   process.exit(0);
 }
 
 let filepath = process.argv[2];
-if(!fs.existsSync(filepath)) {
-  console.error("Invalid filepath "+filepath);
+if (!fs.existsSync(filepath)) {
+  console.error("Invalid filepath " + filepath);
   process.exit(0);
 }
 
-if(fs.lstatSync(filepath).isDirectory()) {
+if (fs.lstatSync(filepath).isDirectory()) {
 
   let blogIndex = {
-    posts : []
+    posts: []
   };
   let filepaths = fs.readdirSync(filepath);
-  for(let fname of filepaths) {
-    bakePost(filepath+path.sep+fname, blogIndex);
+  for (let fname of filepaths) {
+    bakePost(filepath + path.sep + fname, blogIndex);
   }
 
-  fs.writeFileSync("blogindex.json", JSON.stringify(blogIndex,null,2));
+  fs.writeFileSync("blogindex.json", JSON.stringify(blogIndex, null, 2));
 
 } else {
   bakePost(filepath);
@@ -37,12 +37,12 @@ if(fs.lstatSync(filepath).isDirectory()) {
 function bakePost(mdFilepath, blogIndex) {
 
   // Parse post mdFilepath and deduce it's date
-  let lastSlashIndex = mdFilepath.lastIndexOf("/");
-  let filename = mdFilepath.substring(lastSlashIndex+1);
+  let lastSlashIndex = mdFilepath.lastIndexOf(path.sep);
+  let filename = mdFilepath.substring(lastSlashIndex + 1);
   let regex = /^(\d+)-(\d+)-(\d+)-([a-zA-Z0-9\-]+)/;
 
-  if(!regex.test(filename)) {
-    console.log("Ignoring: "+filename);
+  if (!regex.test(filename)) {
+    console.log("Ignoring: " + filename);
     return;
   }
 
@@ -58,7 +58,7 @@ function bakePost(mdFilepath, blogIndex) {
   let dashedPostName = match[4];
 
 
-  let dateString = months[month-1]+" "+parseInt(dayStr)+", "+yearStr;
+  let dateString = months[month - 1] + " " + parseInt(dayStr) + ", " + yearStr;
 
   // Extract title of the post
   let postTitle = null;
@@ -66,17 +66,17 @@ function bakePost(mdFilepath, blogIndex) {
   let bodyStart = false;
   let categories = [];
   let keywords = [];
-  for(let line of mdInput.split(/\r?\n/)) {
-    if(bodyStart) {
-      mdBody += line+"\n";
+  for (let line of mdInput.split(/\r?\n/)) {
+    if (bodyStart) {
+      mdBody += line + "\n";
     } else {
-      if(line.trim().startsWith("categories:")) {
-        categories = line.substring(line.indexOf(":")+1).split(",");
-      } else if(line.trim().startsWith("keywords:")) {
-        keywords = line.substring(line.indexOf(":")+1).split(",");
-      } else if(line.trim().startsWith("===")) {
+      if (line.trim().startsWith("categories:")) {
+        categories = line.substring(line.indexOf(":") + 1).split(",");
+      } else if (line.trim().startsWith("keywords:")) {
+        keywords = line.substring(line.indexOf(":") + 1).split(",");
+      } else if (line.trim().startsWith("===")) {
         bodyStart = true;
-      } else if(line.trim().length > 0 && postTitle === null) {
+      } else if (line.trim().length > 0 && postTitle === null) {
         postTitle = line;
       }
     }
@@ -89,43 +89,43 @@ function bakePost(mdFilepath, blogIndex) {
   // Generate html from markdown
   let converter = new showdown.Converter();
 
-  let postHead = postHeadTemplate.replace(/\$TITLE\$/g,postTitle);
-  postHead = postHead.replace('$DESCRIPTION$',mdBody.substr(0,150));
-  postHead = postHead.replace('$KEYWORDS$',keywords.join(','));
+  let postHead = postHeadTemplate.replace(/\$TITLE\$/g, postTitle);
+  postHead = postHead.replace('$DESCRIPTION$', mdBody.substr(0, 150));
+  postHead = postHead.replace('$KEYWORDS$', keywords.join(','));
   let htmlOutput = converter.makeHtml(mdBody);
 
-  let postBody = postBodyTemplate.replace('$ARTICLE$',htmlOutput);
+  let postBody = postBodyTemplate.replace('$ARTICLE$', htmlOutput);
   postBody = postBody.replace('$POSTDATE$', dateString);
-  postBody = postBody.replace(/\$TITLE\$/g,postTitle);
+  postBody = postBody.replace(/\$TITLE\$/g, postTitle);
 
   // Create directory structure, if necessary
-  let outpath = ""+yearStr;
-  if(!fs.existsSync(outpath)) {
+  let outpath = "" + yearStr;
+  if (!fs.existsSync(outpath)) {
     fs.mkdirSync(outpath);
   }
-  outpath += path.sep+monthStr;
-  if(!fs.existsSync(outpath)) {
+  outpath += path.sep + monthStr;
+  if (!fs.existsSync(outpath)) {
     fs.mkdirSync(outpath);
   }
-  outpath += path.sep+dayStr;
-  if(!fs.existsSync(outpath)) {
+  outpath += path.sep + dayStr;
+  if (!fs.existsSync(outpath)) {
     fs.mkdirSync(outpath);
   }
-  outpath += path.sep+dashedPostName+".html";
+  outpath += path.sep + dashedPostName + ".html";
 
-  if(blogIndex != null) {
+  if (blogIndex != null) {
     blogIndex.posts.push({
-      title : postTitle,
-      path : outpath,
-      month : month,
-      year : parseInt(yearStr),
-      day : parseInt(dayStr),
-      keywords : keywords,
-      categories : categories
+      title: postTitle,
+      path: outpath,
+      month: month,
+      year: parseInt(yearStr),
+      day: parseInt(dayStr),
+      keywords: keywords,
+      categories: categories
     });
   }
-  fs.writeFileSync(outpath, postHead+"\n"+postBody);
+  fs.writeFileSync(outpath, postHead + "\n" + postBody);
 
-  console.log("Output html: "+outpath);
+  console.log("Output html: " + outpath);
 }
 
